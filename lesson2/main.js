5acc68cd24e41c80d038edc4a171c2b9337b51fb
tho <nguyendangtho@gmail.com>
2026-06-24 16:57:56 +0700

    fix_lesson3

--- lesson2/main.js
+++ lesson2/main.js
@@ -2,37 +2,52 @@ const diemInput = document.getElementById("diem");
 const kiemTra = document.getElementById("kiemTra");
 const ketQua = document.getElementById("ketQua");
 
-kiemTra.addEventListener("click", function(){
-    const diem = Number(diemInput.value);
-    if (diemInput.value === "") {
-        ketQua.textContent = "Vui lòng nhập điểm trước khi kiểm tra!";
-        ketQua.style.color = "orange"; // Đổi chữ thành màu cam để cảnh báo
-        return; // Dừng hàm lại không chạy tiếp xuống dưới
-    }
+kiemTra.addEventListener("click", function () {
+  const diem = Number(diemInput.value);
+  if (diemInput.value === "") {
+    ketQua.textContent = "Vui lòng nhập điểm trước khi kiểm tra!";
+    ketQua.style.color = "orange"; // Đổi chữ thành màu cam để cảnh báo
+    return; // Dừng hàm lại không chạy tiếp xuống dưới
+  }
 
-    // Bước 5: Sử dụng cấu trúc if - else if - else để phân loại điểm
-    if (diem >= 8) {
-        ketQua.textContent = "Xuất sắc! 🎉";
-        ketQua.style.color = "green"; // Đổi chữ thành màu xanh lá
-    } 
-    else if (diem >= 5 && diem < 8) {
-        ketQua.textContent = "Đạt yêu cầu 👍";
-        ketQua.style.color = "blue"; // Đổi chữ thành màu xanh dương
-    } 
-    else {
-        ketQua.textContent = "Cần cố gắng hơn 😢";
-        ketQua.style.color = "red"; // Đổi chữ thành màu đỏ
-    }
+  // Bước 5: Sử dụng cấu trúc if - else if - else để phân loại điểm
+  if (diem >= 8) {
+    ketQua.textContent = "Xuất sắc! 🎉";
+    ketQua.style.color = "green"; // Đổi chữ thành màu xanh lá
+  } else if (diem >= 5 && diem < 8) {
+    ketQua.textContent = "Đạt yêu cầu 👍";
+    ketQua.style.color = "blue"; // Đổi chữ thành màu xanh dương
+  } else {
+    ketQua.textContent = "Cần cố gắng hơn 😢";
+    ketQua.style.color = "red"; // Đổi chữ thành màu đỏ
+  }
 });
 
+// Nhập birthday tự động hiện tuổi
 const birthday = document.getElementById("birthday");
 const showAgeInput = document.getElementById("showAge");
 
-const currentYear = new Date().getFullYear();
+birthday.addEventListener("change", function () {
+  const currentYear = new Date().getFullYear();
 
-const ngaySinhUser = new Date(birthday.value);
-const namSinh = ngaySinhUser.getFullYear;
+  const ngaySinhUser = new Date(birthday.value);
+  const namSinh = ngaySinhUser.getFullYear();
 
-const age = currentYear - namSinh ;
+  const age = currentYear - namSinh;
 
-showAgeInput.value = age;
+  showAgeInput.value = age;
+});
+
+//Xuất dữ liệu thông tin User
+const nameInput = document.getElementById("name");
+const phoneInput = document.getElementById("phone");
+const xuatDuLieu = document.getElementById("btn");
+
+xuatDuLieu.addEventListener("click", function (){
+    const name = nameInput.value;
+    const phone = phoneInput.value;
+    if (phone === "" || name === ""){
+        alter("Cảnh báo: Bạn nhập thiếu tên hoặc số điện thoại!");
+        return;
+    }
+});
